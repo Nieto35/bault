840778a0a4900e01f35b942fbfec0bed615df437
Nieto35 <jfnieto00@misena.edu.co>
2025-06-06 11:42:43 +0200

    config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nieto35.github.io',
-    base: 'astro_github_pages_ejemplo',
+    base: 'bault',
 })
